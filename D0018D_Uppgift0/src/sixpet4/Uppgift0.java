1eb228bbe654ff6dbeb0e1b99208ae16ee62d33d
Sixten Peterson <sixten.peterson@winterzone.se>
2025-02-19 13:42:24 +0100

    Initial commit

---
+++ D0018D_Uppgift0/src/sixpet4/Uppgift0.java
@@ -0,0 +1,14 @@
+package sixpet4;
+
+/**
+ * Prints "Hej Värld!" to the terminal.
+ * @author Sixten Peterson, sixpet-4
+ * @version 1.0
+ */
+public class Uppgift0 {
+	
+	public static void main(String[] args)
+	{
+		System.out.println("Hej Värld!");
+	}
+}
